e63f6212eeb63ad9998e261407e52bae59308fe3
easypx <r_sachse@gmx.de>
2023-08-01 16:38:23 +0200

    Update

--- source/helpers.cpp
+++ source/helpers.cpp
@@ -55,33 +55,4 @@ namespace Klangraum
         return static_cast<int32_t>(floatValue * static_cast<float>(INT32_MAX));
     }
 
-    // Wandle alle Elemente in Kleinbuchstaben um
-    // NOTE: tolower() ist Lambda Funktion in transform()
-    /*	std::transform(lines.begin(), lines.end(), lines.begin(), [](std::string str)
-                       {
-    for (char& c : str) {
-        c = std::tolower(c);
-    }
-    return str; });
-
-    float g_fScale = 2.0f / 0xffffffff;
-int g_x1 = 0x67452301;
-int g_x2 = 0xefcdab89;
-
-void whitenoise(
-  float* _fpDstBuffer, // Pointer to buffer
-  unsigned int _uiBufferSize, // Size of buffer
-  float _fLevel ) // Noiselevel (0.0 ... 1.0)
-{
-  _fLevel *= g_fScale;
-
-  while( _uiBufferSize-- )
-  {
-    g_x1 ^= g_x2;
-    *_fpDstBuffer++ = g_x2 * _fLevel;
-    g_x2 += g_x1;
-  }
-}
-*/
-
 } // namespace Klangraum
